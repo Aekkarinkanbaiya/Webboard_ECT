41aa8dfcde4232a915d18cc0ffa0658ba895d754
Aekkarin <tawangold78@gmail.com>
2024-03-01 22:28:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -49,6 +49,11 @@ session_start();
         INNER JOIN user as t2 ON (t1.user_id=t2.id)
         INNER JOIN category as t3 ON (t1.cat_id=t3.id) ORDER BY t1.post_date DESC";
         $result=$conn->query($sql);
+        while($row = $result->fetch()){
+          echo "<tr><td>[ $row[0] ] <a href=post.php?id=$row[2]
+          style=text-decoration:none>$row[1]</a><br>$row[3] - $row[4]</td></tr>";
+        }
+        $conn=null;
         ?>
   </table>
     </div>
